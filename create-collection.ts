691a783ba1f91cd32258112f183dd66a93bf9d79
Goodbadboy <selfmanademola@gmail.com>
2026-02-02 09:39:35 +0100

    import the neccesary imports needed

--- create-collection.ts
+++ create-collection.ts
@@ -1,3 +1,4 @@
 import { createNft, fetchDigitalAsset, mplTokenMetadata } from "@metaplex-foundation/mpl-token-metadata";
 import { airdropIfRequired, getExplorerLink, getKeypairFromFile } from "@solana-developers/helpers";
 import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
+import { Connection, LAMPORTS_PER_SOL } from "@solana/web3.js";
