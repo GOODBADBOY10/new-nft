41671558191ec3aaedff0c2f063bbeeb899a2889
Goodbadboy <selfmanademola@gmail.com>
2026-02-02 09:40:35 +0100

    create a connection to solana

--- create-collection.ts
+++ create-collection.ts
@@ -1,4 +1,6 @@
 import { createNft, fetchDigitalAsset, mplTokenMetadata } from "@metaplex-foundation/mpl-token-metadata";
 import { airdropIfRequired, getExplorerLink, getKeypairFromFile } from "@solana-developers/helpers";
 import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
-import { Connection, LAMPORTS_PER_SOL } from "@solana/web3.js";
+import { clusterApiUrl, Connection, LAMPORTS_PER_SOL } from "@solana/web3.js";
+
+const connection = new Connection(clusterApiUrl("devnet"));
