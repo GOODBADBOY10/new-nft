e8fffe2a6f0ca845d9e398db0ea41691c5ac5efd
Goodbadboy <selfmanademola@gmail.com>
2026-02-02 09:37:44 +0100

    created a collections file

--- create-collection.ts
+++ create-collection.ts
@@ -1,1 +1,3 @@
 import { createNft, fetchDigitalAsset, mplTokenMetadata } from "@metaplex-foundation/mpl-token-metadata";
+import { airdropIfRequired, getExplorerLink, getKeypairFromFile } from "@solana-developers/helpers";
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
