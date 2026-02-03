9f60f9a425a17c15a50f246efc03eb5e38bbe7a0
Goodbadboy <selfmanademola@gmail.com>
2026-02-03 08:51:44 +0100

    created a user and log the user

--- create-collection.ts
+++ create-collection.ts
@@ -4,3 +4,9 @@ import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
 import { clusterApiUrl, Connection, LAMPORTS_PER_SOL } from "@solana/web3.js";
 
 const connection = new Connection(clusterApiUrl("devnet"));
+
+const user = await getKeypairFromFile();
+
+await airdropIfRequired(connection, user.publicKey, 1 * LAMPORTS_PER_SOL, 0.5 * LAMPORTS_PER_SOL);
+
+console.log("Loaded user", user.publicKey.toBase58());
