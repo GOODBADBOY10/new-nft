a256d7e6c7cd401508e0f02bbf7dbee116dc0205
Goodbadboy <selfmanademola@gmail.com>
2026-02-04 09:17:34 +0100

    create a collection addres

--- create-nft.ts
+++ create-nft.ts
@@ -1,5 +1,7 @@
 import { createNft, mplTokenMetadata } from "@metaplex-foundation/mpl-token-metadata";
 import { airdropIfRequired, getExplorerLink, getKeypairFromFile } from "@solana-developers/helpers";
 import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
-import { clusterApiUrl, Connection, LAMPORTS_PER_SOL } from "@solana/web3.js";
+import { clusterApiUrl, Connection, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js";
 import { generateSigner, keypairIdentity, percentAmount } from "@metaplex-foundation/umi";
+
+const collectionAddress = new PublicKey("Ursg8AWN7SxRKE5xQQWize17TtjptGPRxJKs82hqg2p")
